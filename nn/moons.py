0136528585aedbea019d3f0b5a9a5c70e4ea52f9
Matt Salo <mattsalo@gmail.com>
2018-01-30 21:26:25 -0600

    comments in moons

--- nn/moons.py
+++ nn/moons.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #np.random.seed(0)
-X, y = sklearn.datasets.make_moons(200, noise=.15)
+X, y = sklearn.datasets.make_moons(200, noise=.5)
 plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
 # plt.show()
 
